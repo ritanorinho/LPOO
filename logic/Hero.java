733c8d805aee1e7965841cc703ad034ed3b5f1f1
Ana Rita Norinho <up201606003@fe.up.pt>
2018-03-30 15:42:23 +0100

    Add files via upload

--- logic/Hero.java
+++ logic/Hero.java
@@ -2,7 +2,6 @@ package dkeep.logic;
 
 public class Hero extends Character {
 	public boolean hasKey=false;
-	//public boolean isArmed = false;
 	 
 	public CellPosition position() {
 		CellPosition pos= new CellPosition(x,y);
